60338a03910931bda110db13d2829ef63416f787
Tim Peters <tim@wirelab.nl>
2016-02-08 10:16:10 +0100

    Add Monkey Microgem
    Monkey is for Sending mails with the mandrill API

---
+++ monkey.gemspec
@@ -0,0 +1,9 @@
+GEM::Specification.new do |gem|
+  gem.name = gem.summary = 'Monkey'
+  gem.version = '1.0.0'
+  gem.require_path = 'monkey'
+  gem.files = `git ls-files monkey`.split("\n")
+  gem.authors = ["Tim Peters"]
+
+  gem.add_dependency 'mandrill-api', '~> 1.0.53'
+end
---
+++ monkey/monkey.rb
@@ -0,0 +1,52 @@
+require 'mandrill'
+module Monkey
+  class Mandrill
+    def initialize(key:)
+      @client = Mandrill::API.new key
+    end
+
+    def send_template template, queue
+      to = queue.map do |rcpt|
+        {
+          email: rcpt[:email],
+          name: rcpt[:name]
+        }
+      end
+
+      merge_vars = queue.map do |rcpt|
+        {
+          rcpt: rcpt[:email],
+          vars: rcpt[:vars].map do |key, value|
+            {
+              name: key,
+              content: value
+            }
+          end + [{name: 'NAME', content: rcpt[:name]}]
+        }
+      end
+
+      message = {
+        to: to,
+        merge_vars: merge_vars,
+      }
+      @client.messages.send_template template, [], message
+    end
+  end
+
+  class Queue
+    def initialize(template:, client:)
+      @template, @client = template, client
+      @queue = []
+    end
+
+    def queue(email:, name:, **vars)
+      @queue << {email: email, name: name, vars: vars}
+      self
+    end
+
+    def send
+      @client.send_template @template, @queue
+      @queue.clear
+    end
+  end
+end
